58afcf3c0199c0df3ec6f66277e5f509197a8a33
preston <polston13@outlook.com>
2024-09-13 12:06:42 -0400

    defined my variables for servo and potentiometer

--- arduinoServo/arduinoServo.ino
+++ arduinoServo/arduinoServo.ino
@@ -1,11 +1,26 @@
 #include <Servo.h>
+int servoPin = 9;
+int potPin = A0;
+int potVal;
+float voltToAngle;
+int servoControl = 9;
+int servoAngle;
+int br =115200;
+int delayTime = 25;
+String prompt = "Rotate the potentiometer to move the servo. ";
+String potPosition = "Potentiometer position is: ";
+String servoPosition = "Servo position is: "
+Servo myServo;
 
 void setup() {
-  // put your setup code here, to run once:
+pinMode(potPin, INPUT);
+pinMode(servoControl, OUTPUT)
+Serial.begin(br);
+myServo.attach(servoPin);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
